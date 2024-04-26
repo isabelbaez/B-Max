69b4b9d4b7f0c9a58622a7f2e3a2ab097cffec58
Isabel Baez <ibaez099@gmail.com>
2024-04-25 21:40:00 -0400

    Adds the Option to Re-record Survey Options

--- src/components/AudioRecorder.js
+++ src/components/AudioRecorder.js
@@ -4,6 +4,7 @@ import React, { useState, useRef, useEffect } from 'react';
 function AudioRecorder({ state, setState, allAnswers, setAllAnswers }) {
     const [isRecording, setIsRecording] = useState(false);
     const [isAnswering, setIsAnswering] = useState(false);
+    const [hasAnswered, setHasAnswered] = useState(false);
     const [questions, setQuestions] = useState(null);
     const [currQuestion, setCurrQuestion] = useState(1);
     const [currTrancription, setCurrTrancription] = useState(null);
@@ -37,8 +38,6 @@ function AudioRecorder({ state, setState, allAnswers, setAllAnswers }) {
         .then((stream) => {
             audioRef.current.srcObject = stream;
             const mediaRecorder = new MediaRecorder(stream, { mimeType: 'audio/webm' });
-            // const options = { mimeType: 'audio/wav' };
-            // const mediaRecorder = new MediaRecorder(stream, options);
             mediaRecorderRef.current = mediaRecorder;
             mediaRecorder.ondataavailable = (e) => {
             chunksRef.current.push(e.data);
@@ -60,16 +59,22 @@ function AudioRecorder({ state, setState, allAnswers, setAllAnswers }) {
             mediaRecorderRef.current.stop();
         }
         setShowingTranscription(true);
+        setHasAnswered(true);
     };
 
     const nextQuestion = () => {
         setCurrQuestion(currQuestion + 1)
-        // if (currQuestion > 10){
-        //     setState(state + 1)
-        // }
         setShowingTranscription(false);
         setCurrTrancription("");
         setIsAnswering(false);
+        setHasAnswered(false);
+        if (currTrancription != 'Google Speech Recognition could not understand the audio'){
+            allAnswers.push(currTrancription)
+        }
+        else{
+            allAnswers.push('N/A')
+        }
+        setAllAnswers(allAnswers)
     };
 
     const finishSurvey = () => {
@@ -93,13 +98,6 @@ function AudioRecorder({ state, setState, allAnswers, setAllAnswers }) {
           if (response.ok) {
             const json_data = await response.json();
             setCurrTrancription(json_data['transcription']);
-            if (json_data['transcription'] != 'Google Speech Recognition could not understand the audio'){
-                allAnswers.push(json_data['transcription'])
-            }
-            else{
-                allAnswers.push('N/A')
-            }
-            setAllAnswers(allAnswers)
             console.log('Transcoding successful');
           } else {
             console.error('Transcoding failed:', response.statusText);
@@ -139,15 +137,32 @@ function AudioRecorder({ state, setState, allAnswers, setAllAnswers }) {
                                     <b>Stop Recording</b>
                                 </button>
                             ) : (
-                                <button className="recordBtn" onClick={startRecording} disabled={showingTranscription}>
-                                    <svg height="40" viewBox="0 0 32 32" width="40" xmlns="http://www.w3.org/2000/svg">
-                                        <g id="Ikon">
-                                            <circle cx="16" cy="16" fill="#eee" r="14"/>
-                                            <circle cx="16" cy="16" fill={showingTranscription? "#eee" : "#f44336"} r="5"/>
-                                        </g>
-                                    </svg>
-                                    <b>Record Answer</b>
-                                </button>
+                                <div>
+                                    {hasAnswered? (
+                                        <button className="recordBtn" onClick={startRecording}>
+                                            <svg height="40" viewBox="0 0 32 32" width="40" xmlns="http://www.w3.org/2000/svg">
+                                                <g id="Ikon">
+                                                    <circle cx="16" cy="16" fill="#eee" r="14"> a ver </circle>
+                                                    <svg height="30" viewBox="-22 -12 100 80" width="30" xmlns="http://www.w3.org/2000/svg" id="fi_6066733"><g id="Layer_3" fill="#6d6e71" data-name="Layer 3"><path d="m60.42035 35.46243a28.63547 28.63547 0 0 1 -57.05708-3.87781 1.00453 1.00453 0 0 1 1.00767-.9767h3.97342a1.00513 1.00513 0 0 1 .99664 1.03776 22.689 22.689 0 1 0 10.5759-18.79675 1.00465 1.00465 0 0 0 .22414 1.79451l6.4972 2.20177a1 1 0 0 1 -.15942 1.934l-17.51426 2.86664a1 1 0 0 1 -1.1609-.95147l-.56589-15.97962a1 1 0 0 1 1.87062-.52624l3.10929 5.519a1.00479 1.00479 0 0 0 1.52448.26524 28.61444 28.61444 0 0 1 46.67819 25.48967z"></path><path d="m60.42035 35.46243a28.63547 28.63547 0 0 1 -57.05708-3.87781 1.00453 1.00453 0 0 1 1.00767-.9767h3.97342a1.00513 1.00513 0 0 1 .99664 1.03776 22.689 22.689 0 1 0 10.5759-18.79675 1.00465 1.00465 0 0 0 .22414 1.79451l6.4972 2.20177a1 1 0 0 1 -.15942 1.934l-17.51426 2.86664a1 1 0 0 1 -1.1609-.95147l-.56589-15.97962a1 1 0 0 1 1.87062-.52624l3.10929 5.519a1.00479 1.00479 0 0 0 1.52448.26524 28.61444 28.61444 0 0 1 46.67819 25.48967z"></path></g><g id="Icon"><path d="m42.05469 4.12988a29.4416 29.4416 0 0 0 -28.96582 5.08692l-3.10938-5.51953a2.00014 2.00014 0 0 0 -3.74121 1.05322l.56641 15.97951a1.99907 1.99907 0 0 0 2.32131 1.90281l17.51464-2.8667a2.00033 2.00033 0 0 0 .31936-3.86811l-6.50879-2.20361a21.65076 21.65076 0 1 1 6.46192 39.36718 21.80263 21.80263 0 0 1 -16.57231-21.39897 2.01389 2.01389 0 0 0 -1.99609-2.05469h-3.97364a2.0077 2.0077 0 0 0 -2.00781 1.96143 29.63241 29.63241 0 0 0 46.48047 24.79785 29.38226 29.38226 0 0 0 12.56836-20.77979 29.68216 29.68216 0 0 0 -19.35742-31.45752z" fill="#e53935"></path></g></svg>
+
+                                                </g>
+                                            </svg>
+                                            <b>Re-record</b>
+                                        </button>
+                                    ) : (
+                                        <button className="recordBtn" onClick={startRecording}>
+                                            <svg height="40" viewBox="0 0 32 32" width="40" xmlns="http://www.w3.org/2000/svg">
+                                                <g id="Ikon">
+                                                    <circle cx="16" cy="16" fill="#eee" r="14"/>
+                                                    <circle cx="16" cy="16" fill={"#f44336"} r="5"/>
+                                                </g>
+                                            </svg>
+                                            <b>Record Answer</b>
+
+                                            
+                                        </button>
+                                    )}
+                                </div>
                             )}
                             
                             <span>{showingTranscription && currTrancription}</span>
@@ -159,8 +174,6 @@ function AudioRecorder({ state, setState, allAnswers, setAllAnswers }) {
                             ) : (
                                 <button className="flowBtn" style={{display: showingTranscription ? "" : "none"}} onClick={nextQuestion}>
                                     <b>Next Question</b>
-                                    {/* <iframe src="src/icons/right-arrow.svg" width="100" height="100" frameBorder="0"></iframe>
-                                    <img src="src/icons/right-arrow.svg" alt="right-arrow"></img> */}
                                 </button>
                             )}
                         </div>
